9c4c0b344d373a92eace14cd9caefbafbc8450df
lisandroman <lisandromansilla.dg@gmail.com>
2023-01-28 16:51:42 +0200

    Fix Players List. Map Added

--- src/components/pages/BuyPlayers/BuyPlayers.jsx
+++ src/components/pages/BuyPlayers/BuyPlayers.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { PlayerPrices } from "../../data/playerPrices.data";
 
 const BuyPlayers = () => {
   return (
@@ -8,7 +9,7 @@ const BuyPlayers = () => {
       <table className="table table-secondary table-striped">
         <thead>
           <tr>
-            <th scope="col">#</th>
+            <th scope="col"></th>
             <th scope="col">Player Name</th>
             <th scope="col">Rating</th>
             <th scope="col">Position</th>
@@ -17,36 +18,20 @@ const BuyPlayers = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Pelé</td>
-            <td>98</td>
-            <td>CAM</td>
-            <td>ASK</td>
-            <td>
-              <button className="btn btn-primary">ask@payment.com</button>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Ronaldo</td>
-            <td>96</td>
-            <td>ST</td>
-            <td>ASK</td>
-            <td>
-              <button className="btn btn-primary">ask@payment.com</button>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Zinedine Zidane</td>
-            <td>96</td>
-            <td>CAM</td>
-            <td>ASK</td>
-            <td>
-              <button className="btn btn-primary">ask@payment.com</button>
-            </td>
-          </tr>
+          {PlayerPrices.map((item) => (
+            <tr key={item.id} className="align-middle">
+              <th scope="row">
+                <img src={item.image} alt="player" style={{width:"4rem"}} />
+              </th>
+              <td>{item.playerName}</td>
+              <td>{item.rating}</td>
+              <td>{item.position}</td>
+              <td>{item.price}</td>
+              <td>
+                <button className="btn btn-primary">ask@payment.com</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </BuyPlayersPageStyled>
