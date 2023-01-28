35bf654c7999e508acc65f0ee94cc6068086ae90
lisandroman <lisandromansilla.dg@gmail.com>
2023-01-28 17:04:15 +0200

    Finished Players Page

--- src/components/pages/BuyPlayers/BuyPlayers.jsx
+++ src/components/pages/BuyPlayers/BuyPlayers.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 import { PlayerPrices } from "../../data/playerPrices.data";
+import Swal from "sweetalert2";
 
 const BuyPlayers = () => {
+
+  const handleButtonAskPlayer = (playerName, rating) => {
+     Swal.fire({
+      icon: "success",
+      title: `To get your ${rating} ${playerName}` ,
+      text: `Send an email to: ask@payment.com`,
+      footer: '<a href="/">Home</a>',
+    });
+  }
   return (
     <BuyPlayersPageStyled>
-      <h2>Buy your favorite FUT Player</h2>
+      <h2 className="text-primary mb-0">We teach you</h2>
+      <h3 className="text-primary mb-4">How to reach your favorite FUT players</h3>
       <table className="table table-secondary table-striped">
         <thead>
           <tr>
@@ -21,14 +32,16 @@ const BuyPlayers = () => {
           {PlayerPrices.map((item) => (
             <tr key={item.id} className="align-middle">
               <th scope="row">
-                <img src={item.image} alt="player" style={{width:"4rem"}} />
+                <img src={item.image} alt="player" style={{ width: "4rem" }} />
               </th>
               <td>{item.playerName}</td>
               <td>{item.rating}</td>
               <td>{item.position}</td>
               <td>{item.price}</td>
               <td>
-                <button className="btn btn-primary">ask@payment.com</button>
+                <button className="btn btn-primary"
+                onClick={()=>handleButtonAskPlayer(item.playerName,item.rating)}
+                >ask@payment.com</button>
               </td>
             </tr>
           ))}
