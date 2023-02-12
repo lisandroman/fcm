6e6908d830072edc41046e0474d2ecfae3beff67
lisandroman <lisandromansilla.dg@gmail.com>
2023-02-12 22:33:45 +0200

    1USD added

--- src/components/data/featuredProducts.data.jsx
+++ src/components/data/featuredProducts.data.jsx
@@ -5,6 +5,13 @@ export const FeaturedProductsData = [
   {
     id: uuidv4(),
     platform: "PS4/5",
+    coins: 1,
+    price: 1,
+    image: bg_card_fs,
+  },
+  {
+    id: uuidv4(),
+    platform: "PS4/5",
     coins: 250000,
     price: 22.5,
     image: bg_card_fs,
@@ -14,87 +21,76 @@ export const FeaturedProductsData = [
     platform: "PS4/5",
     coins: 500000,
     price: 45,
-    image:
-      bg_card_fs,
+    image: bg_card_fs,
   },
   {
     id: uuidv4(),
     platform: "PS4/5",
     coins: 1000000,
     price: 70,
-    image:
-      bg_card_fs,
+    image: bg_card_fs,
   },
   {
     id: uuidv4(),
     platform: "PS4/5",
     coins: 2000000,
     price: 140,
-    image:
-      bg_card_fs,
+    image: bg_card_fs,
   },
   {
     id: uuidv4(),
     platform: "XB",
     coins: 250000,
     price: 22.5,
-    image:
-      bg_card_fs,
+    image: bg_card_fs,
   },
   {
     id: uuidv4(),
     platform: "XB",
     coins: 500000,
     price: 45,
-    image:
-      bg_card_fs,
+    image: bg_card_fs,
   },
   {
     id: uuidv4(),
     platform: "XB",
     coins: 1000000,
     price: 70,
-    image:
-      bg_card_fs,
+    image: bg_card_fs,
   },
   {
     id: uuidv4(),
     platform: "XB",
     coins: 2000000,
     price: 140,
-    image:
-      bg_card_fs,
+    image: bg_card_fs,
   },
   {
     id: uuidv4(),
     platform: "PC",
     coins: 250000,
     price: 16.67,
-    image:
-      bg_card_fs,
+    image: bg_card_fs,
   },
   {
     id: uuidv4(),
     platform: "PC",
     coins: 500000,
     price: 30,
-    image:
-      bg_card_fs,
+    image: bg_card_fs,
   },
   {
     id: uuidv4(),
     platform: "PC",
     coins: 1000000,
     price: 55.55,
-    image:
-      bg_card_fs,
+    image: bg_card_fs,
   },
   {
     id: uuidv4(),
     platform: "PC",
     coins: 2000000,
     price: 111.11,
-    image:
-      bg_card_fs,
+    image: bg_card_fs,
   },
 ];
