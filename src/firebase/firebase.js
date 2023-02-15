aed1b7c3229d28f9d24d80cdeff6c8f6f1908e42
lisandroman <lisandromansilla.dg@gmail.com>
2023-02-15 10:35:10 +0200

    Fake apiKey to test

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId,
+  apiKey: "AIzaSyBPo5bFCbxOfK6GuBojUHHpCAISrvW3LRc",
+  authDomain: "futcoinsmarket-d27b0.firebaseapp.com",
+  projectId: "futcoinsmarket-d27b0",
+  storageBucket: "futcoinsmarket-d27b0.appspot.com",
+  messagingSenderId: "255150530624",
+  appId: "1:255150530624:web:e10eaa4c92ffa9d0e87fa5",
+  measurementId: "G-5ZKJX4Q7B9",
 };
 
 // Initialize Firebase
